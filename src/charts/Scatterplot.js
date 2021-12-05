90d7ec3aed710b1de34f6c76c33a63d5f809b392
wcwalla <wcwalla@pm.me>
2021-12-05 04:57:56 -0500

    Added zoom

--- src/charts/Scatterplot.js
+++ src/charts/Scatterplot.js
@@ -8,17 +8,17 @@ const drawChart = (data, platforms, region) => {
       return platforms.includes(d['Platform'])
   })
 
-    const margin = {top: 30, right: 115, bottom: 50, left: 50}
-    const width = 1125 - margin.left - margin.right
-    const height = 638 - margin.top - margin.bottom;
-
-  // append the svg object to the body of the page
-  const svg = d3.select("#my_dataviz")
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const margin = {top: 30, right: 115, bottom: 50, left: 50}
+  const width = 1125 - margin.left - margin.right
+  const height = 638 - margin.top - margin.bottom;
+// append the svg object to the body of the page
+  var svg = d3.select("#my_dataviz")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
   svg.append("text")
     .attr("x", (width / 2))
@@ -27,58 +27,22 @@ const drawChart = (data, platforms, region) => {
     .style("font-size", "16px")
     .text("Sales By Region");
 
-    const nintendo = ['Wii', 'GBA', 'GB', 'DS', 'SNES', 'NES', 'WiiU', '3DS', 'GC', 'N64']
-    const sony = ['PS2', 'PS3', 'PSV', 'PSP', 'PS', 'PS4']
-    const microsoft = ['XB', 'X360', 'XOne']
-
-  // const color = d3.scaleOrdinal()
-  //               .domain(["Sports", "Platform", "Racing", "Role-Playing", "Shooter", "Simulation", "Action", "Fighting", "Adventure", "Strategy", "Misc", "Puzzle"])
-  //               .range(["red", "blue", "yellow", "green", "orange", "purple", "pink", "gray", "brown", "lightgreen", "magenta", "cyan"])
+  const nintendo = ['Wii', 'GBA', 'GB', 'DS', 'SNES', 'NES', 'WiiU', '3DS', 'GC', 'N64']
+  const sony = ['PS2', 'PS3', 'PSV', 'PSP', 'PS', 'PS4']
+  const microsoft = ['XB', 'X360', 'XOne']
 
   const color = d3.scaleOrdinal()
                 .domain(["Nintendo", "Sony", "Microsoft"])
                 .range(["red", "blue", "green"])
 
-  // // Handmade legend
-  // svg.append("circle").attr("cx",width + 20).attr("cy", margin.top).attr("r", 5).style("fill", "red")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 15).attr("r", 5).style("fill", "blue")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 30).attr("r", 5).style("fill", "yellow")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 45).attr("r", 5).style("fill", "green")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 60).attr("r", 5).style("fill", "orange")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 75).attr("r", 5).style("fill", "purple")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 90).attr("r", 5).style("fill", "pink")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 105).attr("r", 5).style("fill", "gray")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 120).attr("r", 5).style("fill", "brown")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 135).attr("r", 5).style("fill", "lightgreen")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 150).attr("r", 5).style("fill", "magenta")
-  // svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 165).attr("r", 5).style("fill", "cyan")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top).text("Sport").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 15).text("Platformer").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 30).text("Racing").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 45).text("RPG").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 60).text("Shooter").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 75).text("Simulation").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 90).text("Action").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 105).text("Fighting").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 120).text("Adventure").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 135).text("Strategy").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 150).text("Misc").style("font-size", "15px").attr("alignment-baseline","middle")
-  // svg.append("text").attr("x", width + 30).attr("y", margin.top + 165).text("Puzzle").style("font-size", "15px").attr("alignment-baseline","middle")
-
-  svg.append("circle").attr("cx",width + 20).attr("cy", margin.top).attr("r", 5).style("fill", "red")
-  svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 15).attr("r", 5).style("fill", "blue")
-  svg.append("circle").attr("cx",width + 20).attr("cy",margin.top + 30).attr("r", 5).style("fill", "green")
-  svg.append("text").attr("x", width + 30).attr("y", margin.top).text("Nintendo").style("font-size", "15px").attr("alignment-baseline","middle")
-  svg.append("text").attr("x", width + 30).attr("y", margin.top + 15).text("Sony").style("font-size", "15px").attr("alignment-baseline","middle")
-  svg.append("text").attr("x", width + 30).attr("y", margin.top + 30).text("Microsoft").style("font-size", "15px").attr("alignment-baseline","middle")
-
   // Add X axis
-  const xScale = d3.scaleSymlog()
-          .domain([0, d3.max(platformed_data, function(d) { return +d["NA_Sales"] })])
-          .range([ 0, width]);
+  var x = d3.scaleSymlog()
+    .domain(d3.extent(platformed_data, function(d) { return +d["NA_Sales"] }))
+    .range([ 0, width ]);
 
-
-  var xAxis = d3.axisBottom(xScale)
+  var xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
   var xLabel = svg.append("text")
     .attr("class", "x label")
@@ -87,12 +51,13 @@ const drawChart = (data, platforms, region) => {
     .attr("y", height + 40)
     .text("North America Sales (Millions)");
 
-  // // Add Y axis
-  const yScale = d3.scaleSymlog()
-    .domain([0, d3.max(platformed_data, function(d) { return +d["EU_Sales"] })])
-    .range([height, 0]);
+  // Add Y axis
+  var y = d3.scaleSymlog()
+    .domain(d3.extent(platformed_data, function(d) { return +d["EU_Sales"] }))
+    .range([ height, 0]);
 
-  var yAxis = d3.axisLeft(yScale)
+  var yAxis = svg.append("g")
+    .call(d3.axisLeft(y));
 
   var yLabel = svg.append("text")
     .attr("class", "y label")
@@ -124,7 +89,7 @@ const drawChart = (data, platforms, region) => {
       .style("left", (event.pageX) + 15 + "px")
       .style("top", (event.pageY) + 15 + "px")
   }
-  const mouseout = function(event,d) {
+  const mouseout = function() {
     d3.select(this).transition()
       .duration('200')
       .attr("r", 5);
@@ -134,57 +99,96 @@ const drawChart = (data, platforms, region) => {
       .style("visibility", "hidden")
   }
 
-  // Add dots
-  var dots = svg.append('g')
-                .selectAll("dot")
-                .data(platformed_data)
-                .join("circle")
-                .attr("cx", function (d) { return xScale(+d["NA_Sales"]); } )
-                .attr("cy", function (d) { return yScale(+d["EU_Sales"]); } )
-                .attr("r", 5)
-                .style("fill", function (d) {
-                  if (nintendo.includes(d.Platform)) {
-                    return color("Nintendo")
-                  }
-                  else if (sony.includes(d.Platform)) {
-                    return color("Sony")
-                  }
-                  else {
-                    return color("Microsoft")
-                  }
-                })
-                .style("padding", "10px")
-                .attr("stroke", "#000000")
-                .attr("stroke-width", "1.5")
-                .on('mousemove', mousemove)
-                .on('mouseout', mouseout);
+  // Add a clipPath: everything out of this area won't be drawn.
+  var clip = svg.append("defs").append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("width", width + 5 )
+      .attr("height", height + 5)
+      .attr("x", -1)
+      .attr("y", -4);
 
-  var changing_x_axis = svg.append("g")
-    .attr("transform", "translate("+0+","+height+")")
-    .call(xAxis)
+  // Create the scatter variable: where both the circles and the brush take place
+  var scatter = svg.append('g')
+    .attr("clip-path", "url(#clip)")
 
-  var changing_y_axis = svg.append("g")
-    .call(yAxis)
+  // Add circles
+  scatter
+    .selectAll("circle")
+    .data(platformed_data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return x(+d["NA_Sales"]); } )
+    .attr("cy", function (d) { return y(+d["EU_Sales"]); } )
+    .attr("r", 5)
+    .style("fill", function (d) {
+      if (nintendo.includes(d.Platform)) {
+        return color("Nintendo")
+      }
+      else if (sony.includes(d.Platform)) {
+        return color("Sony")
+      }
+      else {
+        return color("Microsoft")
+      }
+    })
+    .style("padding", "10px")
+    .attr("stroke", "#000000")
+    .attr("stroke-width", "1.5")
+    .on('mousemove', mousemove)
+    .on('mouseout', mouseout);
 
+  // Set the zoom and Pan features: how much you can zoom, on which part, and what to do when there is a zoom
+  var zoom = d3.zoom()
+      .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+      .extent([[0, 0], [width, height]])
+      .on("zoom", function (e) { updateChart(e, "NA_Sales", "EU_Sales") });
+
+  // This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
+  svg.append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .style("fill", "none")
+      .style("pointer-events", "all")
+      .lower()
+      .call(zoom);
+  // now the user can zoom and it will trigger the function called updateChart
+
+  // A function that updates the chart when the user zoom and thus new boundaries are available
+  function updateChart(event, xRegion, yRegion) {
+
+    // recover the new scale
+    var newX = event.transform.rescaleX(x);
+    var newY = event.transform.rescaleY(y);
+
+    // update axes with these new boundaries
+    xAxis.call(d3.axisBottom(newX))
+    yAxis.call(d3.axisLeft(newY))
+
+    // update circle position
+    scatter
+      .selectAll("circle")
+      .attr('cx', function(d) {return newX(d[xRegion])})
+      .attr('cy', function(d) {return newY(d[yRegion])});
+  }
 
   if(region === "naveu"){
     let xRegion = "NA_Sales"
     let yRegion = "EU_Sales"
 
-    xScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[xRegion]}), s => +s)])
-    yScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[yRegion]}), s => +s)])
+    x.domain(d3.extent(platformed_data.map(function(d){return +d[xRegion]}), s => +s))
+    y.domain(d3.extent(platformed_data.map(function(d){return +d[yRegion]}), s => +s))
 
     xLabel
       .text("North America Sales (Millions)")
     yLabel
       .text("Europe Sales (Millions)")
 
-    dots.transition()
-      .duration('500')
-      .attr("cx", function (d) { return xScale(+d[xRegion]); } )
-      .attr("cy", function (d) { return yScale(+d[yRegion]); } )
+    scatter.transition()
+      .duration('1000')
+      .selectAll("circle")
+      .attr("cx", function (d) { return x(+d[xRegion]); } )
+      .attr("cy", function (d) { return y(+d[yRegion]); } )
       .attr("r", 5)
       .style("fill", function (d) {
         if (nintendo.includes(d.Platform)) {
@@ -201,20 +205,18 @@ const drawChart = (data, platforms, region) => {
       .attr("stroke", "#000000")
       .attr("stroke-width", "1.5")
 
-    changing_x_axis.transition()
-      .call(xAxis)
+    xAxis.call(d3.axisBottom(x))
+    yAxis.call(d3.axisLeft(y))
 
-    changing_y_axis.transition()
-      .call(yAxis)
+    zoom
+      .on("zoom", function (e) { updateChart(e, xRegion, yRegion) });
   }
   if(region === "navjp") {
     let xRegion = "NA_Sales"
     let yRegion = "JP_Sales"
 
-    xScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[xRegion]}), s => +s)])
-    yScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[yRegion]}), s => +s)])
+    x.domain(d3.extent(platformed_data.map(function(d){return d[xRegion]}), s => +s))
+    y.domain(d3.extent(platformed_data.map(function(d){return d[yRegion]}), s => +s))
 
     xLabel
       .text("North America Sales (Millions)")
@@ -222,10 +224,11 @@ const drawChart = (data, platforms, region) => {
       .text("Japan Sales (Millions)")
 
 
-    dots.transition()
+    scatter.transition()
       .duration('1000')
-      .attr("cx", function (d) { return xScale(+d[xRegion]); } )
-      .attr("cy", function (d) { return yScale(+d[yRegion]); } )
+      .selectAll("circle")
+      .attr("cx", function (d) { return x(+d[xRegion]); } )
+      .attr("cy", function (d) { return y(+d[yRegion]); } )
       .attr("r", 5)
       .style("fill", function (d) {
         if (nintendo.includes(d.Platform)) {
@@ -242,21 +245,19 @@ const drawChart = (data, platforms, region) => {
       .attr("stroke", "#000000")
       .attr("stroke-width", "1.5")
 
-    changing_x_axis.transition()
-      .call(xAxis)
+    xAxis.call(d3.axisBottom(x))
+    yAxis.call(d3.axisLeft(y))
 
-    changing_y_axis.transition()
-      .call(yAxis)
+    zoom
+      .on("zoom", function (e) { updateChart(e, xRegion, yRegion) });
   }
 
   if(region === "euvjp"){
     let xRegion = "EU_Sales"
     let yRegion = "JP_Sales"
 
-    xScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[xRegion]}), s => +s)])
-    yScale
-        .domain([0, d3.max(platformed_data.map(function(d){return d[yRegion]}), s => +s)])
+    x.domain(d3.extent(platformed_data.map(function(d){return d[xRegion]}), s => +s))
+    y.domain(d3.extent(platformed_data.map(function(d){return d[yRegion]}), s => +s))
 
     xLabel
       .text("Europe Sales (Millions)")
@@ -264,10 +265,11 @@ const drawChart = (data, platforms, region) => {
       .text("Japan Sales (Millions)")
 
 
-    dots.transition()
+    scatter.transition()
       .duration('1000')
-      .attr("cx", function (d) { return xScale(+d[xRegion]); } )
-      .attr("cy", function (d) { return yScale(+d[yRegion]); } )
+      .selectAll("circle")
+      .attr("cx", function (d) { return x(+d[xRegion]); } )
+      .attr("cy", function (d) { return y(+d[yRegion]); } )
       .attr("r", 5)
       .style("fill", function (d) {
         if (nintendo.includes(d.Platform)) {
@@ -284,19 +286,19 @@ const drawChart = (data, platforms, region) => {
       .attr("stroke", "#000000")
       .attr("stroke-width", "1.5")
 
-    changing_x_axis.transition()
-      .call(xAxis)
+    xAxis.call(d3.axisBottom(x))
+    yAxis.call(d3.axisLeft(y))
 
-    changing_y_axis.transition()
-      .call(yAxis)
+    zoom
+      .on("zoom", function (e) { updateChart(e, xRegion, yRegion) });
   }
+
 }
 
 
 const Scatterplot = (props) => {
   const [regions, setRegions] = useState("naveu")
   const handleChange = (event) => {
-    console.log(event.target.value)
     setRegions(event.target.value)
   }
 
